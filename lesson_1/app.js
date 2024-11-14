34c0267ff5f71f774e1fe633d6f66a6dc61a7df5
Alterra$Developer <42990792+AlterraDeveloper@users.noreply.github.com>
2024-11-14 21:18:30 +0600

    add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,9 @@
+console.log("Hello World!");
+// alert("Hello JavaScript");
+document.write("Hello Group 44");
+
+// V8
+
+document.body.style.color = "white";
+document.body.style.fontSize = "6rem";
+document.body.style.fontFamily = "Arial, Helvetica, sans-serif";
